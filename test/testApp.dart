958256fdca8bf7aa467d8b19c2763590de0a1010
BadmanAdom <adomisaac50@gmail.com>
2022-08-03 21:20:23 +0000

    created a unit test/ for checking the textfield and buttons

---
+++ test/testApp.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter_driver/driver_extension.dart';
+// import 'package:package_name/main.dart' as app;
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:bmi/main.dart' as app;
+void main() {
+  enableFlutterDriverExtension();
+
+  app.main();
+}
